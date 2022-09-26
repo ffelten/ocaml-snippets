7cd507a897cf36298065cc562911e02233da74fb
Florian Felten <>
2022-09-26 16:42:26 +0200

    Add locks

---
+++ shared_memory_I/bin/lock_increment_threads.ml
@@ -0,0 +1,21 @@
+let count = ref 0 in
+let lock = Mutex.create () in
+let increment n =
+  let thread_id = Thread.id (Thread.self ()) in
+  for _=0 to n-1 do
+    Mutex.lock lock;
+    let c = !count in
+    Thread.delay 0.5; (* Do something else, like read from file or call http query...*)
+    count := c + 1;
+    Printf.printf "Thread %d incremented from %d to %d\n" thread_id c !count;
+    flush stdout;
+    Mutex.unlock lock;
+  done;
+  ()
+in 
+let t1 = Thread.create increment 10 in
+let t2 = Thread.create increment 10 in
+Thread.join t1;
+Thread.join t2;
+Printf.printf "Result: %d\n" !count
+
