e744137cfc42cfc31e816d76dff05fe38feae9f8
Florian Felten <>
2022-09-26 14:01:13 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ocaml-snippets
-OCaml example codes used for teaching
+OCaml example codes used for teaching PF3 @ Uni.lu.
+
+## Structure
+Examples are divided by lectures, having one dune project by lecture. The example code is located in `bin/` folder.
+
+## Running
+Enter any folder and run  
+1. `$ dune build`
+2. `$ dune exec YOUR_FILE`
---
+++ shared_memory_I/bin/increment_threads.ml
@@ -0,0 +1,18 @@
+let count = ref 0 in
+let increment n =
+  let thread_id = Thread.id (Thread.self ()) in
+  for _=0 to n-1 do
+    let c = !count in
+    Thread.delay 0.5; (* Do something else, like read from file or call http query...*)
+    count := c + 1;
+    Printf.printf "Thread %d incremented from %d to %d\n" thread_id c !count;
+    flush stdout
+  done;
+  ()
+in 
+let t1 = Thread.create increment 10 in
+let t2 = Thread.create increment 10 in
+Thread.join t1;
+Thread.join t2;
+Printf.printf "Result: %d\n" !count
+
