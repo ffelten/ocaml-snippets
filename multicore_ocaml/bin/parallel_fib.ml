dec3d08218b1233bff4bd67bf31e7808845dddbb
Florian Felten <>
2022-09-28 19:12:09 +0200

    Add multicore ocaml snippets

---
+++ multicore_ocaml/bin/parallel_fib.ml
@@ -0,0 +1,11 @@
+let rec fib n = 
+  if n < 2 then 1 
+  else fib (n - 1) + fib (n - 2)
+in
+
+let d1 = Domain.spawn (fun _ -> fib 40) in
+let d2 = Domain.spawn (fun _ -> fib 40) in
+let r1 = Domain.join d1 in
+let r2 = Domain.join d2 in
+Printf.printf "All done!\n";
+Printf.printf "Results: %d - %d\n" r1 r2;
---
+++ multicore_ocaml/bin/pool.ml
@@ -0,0 +1,33 @@
+(* Executing 30 tasks in parallel on n_domains cores *)
+let n_domains = 3 
+let n_tasks = 30 
+
+module Task = Domainslib.Task
+
+let square n = 
+  Thread.delay 0.5;
+  Printf.printf "square %d\n" n;
+  n * n 
+
+let launch_tasks pool = 
+  Array.init n_tasks (fun i -> 
+    Task.async pool (fun _ -> square i)
+  )
+
+let () =
+  (* Instantiating a thread pool *)
+  let pool = Task.setup_pool ~num_additional_domains:(n_domains - 1) () in
+
+  (* Launching the tasks in the pool *)
+  let tasks = launch_tasks pool in
+
+  (* Careful, Task.await should be called inside the Task.run *)
+  Task.run pool (fun _ -> 
+    (* Getting our results *)
+    for i=0 to n_tasks-1 do
+      let result = Task.await pool tasks.(i) in
+      Printf.printf "Result for task %d: %d\n" i result;
+    done;
+  );
+
+  Task.teardown_pool pool;
