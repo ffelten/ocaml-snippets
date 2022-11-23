f5651055ff50f319eff7f2ec12e2ba4c864fc931
Florian Felten <florian.felten@uni.lu>
2022-11-23 09:05:13 +0100

    More explicit logging in philosophers

--- shared_memory_II/bin/philosophers_lock.ml
+++ shared_memory_II/bin/philosophers_lock.ml
@@ -7,11 +7,17 @@ let philosopher id =
   
   let eat () = 
     Mutex.lock mutexes.(left);
+    Printf.printf "Philosopher %d took left fork:%d.\n" id left;
+    flush stdout;
     Thread.delay 0.3;
     Mutex.lock mutexes.(right);
+    Printf.printf "Philosopher %d took right fork:%d.\n" id right;
+    flush stdout;
     Printf.printf "Philosopher %d is eating plate using forks left:%d, right:%d.\n" id left right;
+    flush stdout;
     Thread.delay 0.3;
     Printf.printf "Philosopher %d has finished eating.\n" id;
+    flush stdout;
     Mutex.unlock mutexes.(left);
     Mutex.unlock mutexes.(right);
     flush stdout;
@@ -36,7 +42,7 @@ let philosopher id =
     loop true
 
 let _ = 
-  Printf.printf "Launching";
+  Printf.printf "Launching\n";
   flush stdout;
   let threads = List.init number (fun id -> Thread.create philosopher id) in
   Thread.join (List.hd threads)
